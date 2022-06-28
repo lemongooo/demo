049fd9746df24f118c87aa5b6d853e58f2c79d56
ymm <lemongooo@126.com>
2022-06-28 09:44:44 +0800

    Initial commit

---
+++ src/main/java/com/hiwms/springboot_hiwms/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.hiwms.springboot_hiwms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hiwms/springboot_hiwms/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiwms.springboot_hiwms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
